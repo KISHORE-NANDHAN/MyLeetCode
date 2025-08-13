689f3b7c9e200f1a3b075c55de4e138da7ca248a
KISHORE NANDHAN <155535981+KISHORE-NANDHAN@users.noreply.github.com>
2025-08-13 13:32:14 +0530

    Time: 2 ms (92.67%) | Memory: 47.1 MB (66.21%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int begin = 0;
+        int end = numbers.length - 1;
+        while(begin<end){
+            int sum = numbers[begin]+numbers[end];
+            if(sum == target){
+                return new int[]{(begin+1),(end+1)};
+            }else if(sum < target){
+                begin++;
+            }else{
+                end--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
